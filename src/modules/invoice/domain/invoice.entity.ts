39933bf0c9bed2a1477964b425acce111abd1c7a
Lucian Tavares <lucian.sistemas.sc@gmail.com>
2023-05-13 14:25:31 -0300

    feat: :sparkles: Adiciona UseCase módulo Invoice

--- src/modules/invoice/domain/invoice.entity.ts
+++ src/modules/invoice/domain/invoice.entity.ts
@@ -44,4 +44,8 @@ export default class Invoice extends BaseEntity implements AggregateRoot {
   get items(): Product[] {
     return this._items
   }
+
+  total(): number {
+    return this._items.reduce((acc, item) => acc + item.price, 0);
+  }
 }
